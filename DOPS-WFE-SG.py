46a9e468bba447982bdd5c2bcd26530b092e068f
SyrusHCW <syrushcw@gmail.com>
2019-06-02 16:45:01 -0400

    Create DOPS-WFE-SG.py

---
+++ DOPS-WFE-SG.py
@@ -0,0 +1,95 @@
+import boto3
+from botocore.exceptions import ClientError
+import sys
+import csv
+
+
+region = ['us-east-1', 
+        'us-east-2', 
+        'us-west-1', 
+        'us-west-2',
+        'eu-west-1',
+        'eu-west-2',
+        'eu-west-3']
+
+group_name = 'DOPS-WFE-SG'
+
+csv_name = '{0}{1}'.format(group_name, '.csv')
+
+
+#aws_access_key = sys.argv[1]
+#aws_secret_key = sys.argv[2]
+
+count1 = len(region)
+
+print(group_name)
+
+for z in range(0,count1):
+
+
+    ec2 = boto3.resource('ec2',
+                #aws_access_key_id = aws_access_key ,
+                #aws_secret_access_key = aws_secret_key ,
+                region_name = region[z],
+    )
+
+    client = boto3.client('ec2',
+                #aws_access_key_id = aws_access_key ,
+                #aws_secret_access_key = aws_secret_key ,
+                region_name = region[z],
+    )
+
+
+    sg_id = []
+
+    response = client.describe_security_groups()
+
+    count3 = len(response['SecurityGroups'])
+
+    for x in range(0,count3):
+        if response['SecurityGroups'][x]['GroupName'] == group_name:
+            sg_id.append(response['SecurityGroups'][x]['GroupId'])
+        else:
+            continue
+
+    print(region[z])
+    print(sg_id)
+
+
+    count4 = len(sg_id)
+    for x in range(0,count4):
+        IpPermissions = []
+        path = '{0}{1}'.format('security-groups/', csv_name)
+        f = open(path)
+        csv_f = csv.reader(f)
+        headers = next(csv_f)
+        for row in csv_f:
+                IpPermissions_loop={'IpProtocol': row[0],
+                    'FromPort': int(row[1]),
+                    'ToPort': int(row[2]),
+                    'IpRanges': [{
+                    'CidrIp': row[3], 
+                    'Description': row[4]}]}
+
+                IpPermissions.append(IpPermissions_loop) 
+
+        sg_rule={'IpProtocol': '-1',
+            'FromPort': -1,
+            'ToPort': -1,
+            'UserIdGroupPairs': [{
+            'GroupId': sg_id[x],
+            'Description': 'DevOps Web front end Security Group'}]}
+
+        IpPermissions.append(sg_rule)
+        security_group = ec2.SecurityGroup(sg_id[x])
+        print(IpPermissions)
+        try:
+            security_group.revoke_ingress(IpPermissions=security_group.ip_permissions)
+            data = security_group.authorize_ingress(
+                IpPermissions=IpPermissions)
+        except:
+            print('no inbound rules in this region')
+            data = security_group.authorize_ingress(
+                IpPermissions=IpPermissions)
+
+
